c2bd42cf87ee357edc851d25e3ad3cbdb3639a80
Jorge Arriaga <jrgpoop@ciencias.unam.mx>
2023-05-08 14:53:36 -0600

    join subsamples

--- LUAD/SGCCA/2_3SGCCA_commands.R
+++ LUAD/SGCCA/2_3SGCCA_commands.R
@@ -1,6 +1,6 @@
 #setwd("/home/jarriaga/SGCCA_subsamples")
 setwd("/home/jvasquez/Documents/Lung-Cancer/LUAD/")
-subtypes = c("TRU", "prox.-prolif.", "prox.-inflam")
+subtypes = c("TRU", "prox.-prolif.", "prox.-inflam", "normal")
 
 for (subtype in subtypes) {
   commands = paste("Rscript 2_3SGCCA_Final.R", subtype)
